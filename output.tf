ac7a569250ad37e6370a6f8417c0952cbb06ef58
Flavio Rocha <flavio.rocha16@gmail.com>
2023-05-26 02:44:08 -0300

    fix outputs

--- output.tf
+++ output.tf
@@ -1,19 +1,19 @@
 output "arn" {
-  value       = aws_s3_bucket.s3_bucket.arn
+  value       = aws_s3_bucket.b.arn
   description = "Output ARN S3 Bucket"
 }
 
 output "id" {
-  value       = aws_s3_bucket.s3_bucket.id
+  value       = aws_s3_bucket.b.id
   description = "Output ID S3 Bucket"
 }
 
 output "region" {
-  value       = aws_s3_bucket.s3_bucket.region
+  value       = aws_s3_bucket.b.region
   description = "Output Region S3 Bucket"
 }
 
 output "domain_name" {
-  value       = aws_s3_bucket.s3_bucket.bucket_domain_name
+  value       = aws_s3_bucket.b.bucket_domain_name
   description = "Output Domain name S3 Bucket"
 }
